87ed2e3e3c41ad1a9b40158e095f3e383c0f3f1b
Vishwas <baghel.vishwas0@gmail.com>
2022-04-11 14:45:51 +0530

    [vishwas]add: Flip coin percentage problem

---
+++ BasicProgrammingProb/FlipCoin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgrammingProb
+{
+    public class FlipCoin
+    {
+        const double IS_HEAD = 0.5;
+        int headCount = 0, tailCount = 0;
+        public void Flip()
+        {
+            Console.WriteLine("How many flips do you want ?");
+            int num = Convert.ToInt32(Console.ReadLine());
+            while (num > 0)
+            {
+                Random random = new Random();
+                double option = random.NextDouble();
+                if (option > IS_HEAD)
+                {
+                    headCount++;
+                }
+                else
+                {
+                    tailCount++;
+                }
+                num--;
+            }
+            double headPercentage = (headCount % 100);
+            double tailPercentage = (tailCount % 100);
+            Console.WriteLine("Percentage of Head :" + headPercentage);
+            Console.WriteLine("Percentage of Tail : " + tailPercentage);
+        }
+    }
+}
--- BasicProgrammingProb/Program.cs
+++ BasicProgrammingProb/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BasicProgrammingProb;
+using System;
+namespace BasicProgrammingProb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Basic Programming Problems");
+            bool end = true;
+            Console.WriteLine("1. FlipCoin\n2. End the Program ");
+            while (end)
+            {
+                Console.WriteLine("Choose an option to execute");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin flipcoin = new FlipCoin();
+                        flipcoin.Flip();
+                        break;
+                    case 2:
+                        end = false;
+                        break;
+                    default:
+                        Console.WriteLine("Choose the correct option");
+                        break;
+                }
+            }
+
+        }
+
+    }
+}
